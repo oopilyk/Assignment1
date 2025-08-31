2c8deb47fca25cdad6a3a21ddb8488f6ce2c7985
Jacob Peery <jpeery05@gmail.com>
2025-08-31 18:36:04 -0400

    negate done working on add

--- fixpoint.c
+++ fixpoint.c
@@ -20,35 +20,39 @@ fixpoint_init( fixpoint_t *val, uint32_t whole, uint32_t frac, bool negative ) {
   val->whole = whole;
   val->frac = frac;
   val->negative = negative;
-  // TODO: implement
 }
 
 uint32_t
 fixpoint_get_whole( const fixpoint_t *val ) {
   return val->whole;
-  // TODO: implement
 }
 
 uint32_t
 fixpoint_get_frac( const fixpoint_t *val ) {
   return val->frac;
-  // TODO: implement
 }
 
 bool
 fixpoint_is_negative( const fixpoint_t *val ) {
   return val->negative;
-  // TODO: implement
 }
 
 void
 fixpoint_negate( fixpoint_t *val ) {
-  // TODO: implement
+  if (val->whole == 0 && val->frac == 0) {
+    return;
+  }
+  val->negative = !val->negative;
 }
 
 result_t
 fixpoint_add( fixpoint_t *result, const fixpoint_t *left, const fixpoint_t *right ) {
-  // TODO: implement
+  result->whole = left->whole + right->whole;
+  result->frac = left->frac + right->frac;
+  if (result->whole < left->whole + right->whole) {
+
+  }
+  // TODO: implement Still working on ts!
 }
 
 result_t
